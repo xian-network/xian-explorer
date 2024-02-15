4ef49e43179deb6e34352927095e7c9abc6a47d6
crosschainer <eodkwokm@gmail.com>
2024-02-15 16:24:45 +0000

    rebrand

--- src/components/AppMenu.vue
+++ src/components/AppMenu.vue
@@ -6,7 +6,7 @@ menu.app-menu
     //tm-list-item(to="/nodes" exact @click.native="close" :title="`Full Nodes (${nodes.length})`")
     //tm-list-item(to="/validators" @click.native="close" :title="`Validators (${votingValidators})`" v-bind:class="{ 'active': isValidatorPage }")
     //tm-list-item(to="/validators-revoked" @click.native="close" :title="`Revoked Validators (${revokedValidators})`")
-    //tm-list-item(to="/search" exact @click.native="close" title="Search")
+    tm-list-item(to="/search" exact @click.native="close" title="Search")
 </template>
 
 <script>
